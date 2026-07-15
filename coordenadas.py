f9f903f3f8dd1c2d18e21945bae94c43bd4ae26e
Thomas rodrigues bitencourt <Thomasrbitencourt2@gmail.com>
2026-07-15 16:13:37 -0300

    add informações

--- coordenadas.py
+++ coordenadas.py
@@ -4,4 +4,5 @@ import time
 time.sleep(7)
 print(pa.position())
 
-# fiz isso para conseguir as cordenadas do cursor.
+# fiz isso para conseguir as coordenadas do cursor.
+# ao executar, após 7 segundos, ele diz em qual posição o cursor está
